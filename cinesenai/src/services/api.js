fea7708dfc7be6fd5bb94e9e37fa8f64dbad4c93
Leonardo Souza Leite <leonardoleoleite21@gmail.com>
2026-06-16 21:42:30 -0300

    finalização do projeto, prt front

--- cinesenai/src/services/api.js
+++ cinesenai/src/services/api.js
@@ -58,7 +58,64 @@ export const api = {
       }),
     deletar: (id) =>
       request(`/api/fimles/${id}`, {
-        method: "DELET",
+        method: "DELETE",
+      }),
+  },
+  salas: {
+    listar: () => request("/api/salas"),
+    buscarPorId: (id) => request(`/api/salas/${id}`),
+    criar: (sala) =>
+      request(`/api/salas`, {
+        method: "POST",
+        body: JSON.stringify(sala),
+      }),
+    atualizar: (sala, id) =>
+      request(`/api/salas/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(sala),
+      }),
+    deletar: (id) =>
+      request(`/api/sala/${id}`, {
+        method: "DELETE",
+      }),
+  },
+  sessoes: {
+    listarPorData: (data) => request(`/api/sessoes?data=${data}`),
+    buscarPorFilme: (filmeId) => request(`/api/sessoes?filmeId=${filmeId}`),
+    buscarPorId: (id) => request(`/api/sessoes/${id}`),
+    listarAssentos: (id) => request(`api/sessoes/${id}/assentos`),
+    criar: (sessao) =>
+      request(`/api/sessoes`, {
+        method: "POST",
+        body: JSON.stringify(sessao),
+      }),
+    // Criado por mim :D (N obrigatório).
+    atualizar: (sessao, id) =>
+      request(`/api/sessoes/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(sessao),
+      }),
+    deletar: (id) =>
+      request(`/api/sessoes/${id}`, {
+        method: "DELETE",
+      }),
+  },
+  reservas: {
+    criar: (sessaoId, assentoId) =>
+      request(`/api/reservas`, {
+        method: "POST",
+        body: JSON.stringify({ sessaoId, assentoId }),
+      }),
+    listarMinhas: () => request(`/api/reservas/minhas`),
+    // Criado por mim :D (N obrigatório).
+    atualizar: (reserva, id) =>
+      request(`/api/reservas/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(reserva),
+      }),
+    cancelar: (id) =>
+      request(`/api/reservas/${id}`, {
+        method: "DELETE",
       }),
   },
   avaliacoes: {
@@ -78,14 +135,34 @@ export const api = {
       }),
   },
   favoritos: {
-  listar: () => request("/api/favoritos"),   // TODO
-  adicionar: (filmeId) => request(`/api/${filmeId}/favoritos`,{
-    method: "POST",
-    body: JSON.stringify({filmeId}),
-  }), // TODO
-  remover: (filmeId) => request(`/api/favoritos/${filmeId}`,{
-    method: "DELET",
-  }),   // TODO
-  verificar: (filmeId) => request(`/api/favoritos/verificar?filmeId=${filmeId}`)
+    listar: () => request("/api/favoritos"), // TODO
+    adicionar: (filmeId) =>
+      request(`/api/${filmeId}/favoritos`, {
+        method: "POST",
+        body: JSON.stringify({ filmeId }),
+      }), // TODO
+    remover: (filmeId) =>
+      request(`/api/favoritos/${filmeId}`, {
+        method: "DELETE",
+      }), // TODO
+    verificar: (filmeId) =>
+      request(`/api/favoritos/verificar?filmeId=${filmeId}`),
+  },
+  usuario: {
+    buscarPerfil: () => request("/api/usuario/perfil"),
+    atualizarPerfil: (nome, email) =>
+      request(`/api/usuario/perfil/${(nome, email)}`, {
+        method: "PUT",
+        body: JSON.stringify({ nome, email }),
+      }),
+    trocarSenha: (senhaAtual, novaSenha) =>
+      request(`/api/usuario/perfil${(senhaAtual, novaSenha)}`, {
+        method: "PATCH",
+        body: JSON.stringify({ senhaAtual, novaSenha }),
+      }),
+    excluirConta: (perfilId) =>
+      request(`/api/usuario/perfil/${perfilId}`, {
+        method: "DELETE",
+      }),
   },
 };
